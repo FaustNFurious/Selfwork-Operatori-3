fbbce58209d1c1dc3923e5e1a5282a5ec97d2ce9
FaustNFurious <lorelego2003@gmail.com>
2024-11-11 21:37:19 +0100

    Add files via upload
    Svolto esercizio operatori 3

---
+++ Selfwork Operatori 3/Main.js
@@ -0,0 +1,13 @@
+// Dichiarazione Variabili
+let num1 = 10;
+let num2 = 2;
+
+// Calcoli somma, sottrazione , moltiplicazione , divisione , potenza
+let somma = num1 + num2;
+let sottazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = Math.pow(num1, num2);
+
+// Stampa a schermo dei risultati
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: \nSomma: ${somma} \nSottrazione: ${sottazione} \nMoltiplicazione: ${moltiplicazione} \nDivisione: ${divisione} \nPotenza: ${potenza} `);
